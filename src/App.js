b22511335d8ccf7fc21d54db461facb15dc1c2b5
Thomas0977 <swaranakart@gmail.com>
2023-02-18 08:00:05 -0800

    feat:dark and light mode implemented

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+
+import "./App.css";
+
+// import moon from "./assets/moon.png";
+// import sun from "./assets/sun.png";
+
+import Header from "./Components/Header/Header";
+import Keypad from "./Components/Keypad/Keypad";
 
 function App() {
+
+  const [isDarkMode, setIsDarkMode] = useState(false);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app" data-theme={isDarkMode ? "dark" : ""} >
+
+      <div className="app_calculator">
+
+        <div className="app_calculator_navbar">
+
+          <div
+            className="app_calculator_navbar_toggle"
+            onClick={() => setIsDarkMode(!isDarkMode)}
+          >
+            <div
+              className={`app_calculator_navbar_toggle_circle ${
+                isDarkMode ? "app_calculator_navbar_toggle_circle_active" : " "}`}
+ >
+              {/* <img src={isDarkMode ? moon : sun} alt="mode" /> */}
+            </div>
+          </div>
+        </div>
+        
+        <Header />
+        <Keypad />
+          
+      </div>
     </div>
   );
 }
---
+++ src/Components/Keypad/Keypad.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import './Keypad.css';
+const  Keypad = () => {
+  return (
+    <div className='keypad'>
+        <h1>Key Pad</h1>
+    </div>
+  )
+} 
+export default Keypad;
