db251b36d44df9ae6465a48ee0b04fff8bc1a72b
Thomas0977 <swaranakart@gmail.com>
2023-02-19 01:34:24 -0800

    scrollintoview

--- src/App.js
+++ src/App.js
@@ -2,22 +2,29 @@ import React, { useState } from "react";
 
 import "./App.css";
 
-// import moon from "./assets/moon.png";
-// import sun from "./assets/sun.png";
+import moon from "./assets/moon.png";
+import sun from "./assets/sun.png";
 
 import Header from "./Components/Header/Header";
-import Keypad from "./Components/Keypad/Keypad";
+import KeyPad from "./Components/Keypad/Keypad";
 
 function App() {
-
   const [isDarkMode, setIsDarkMode] = useState(false);
 
+  const handleKeyPress = (keyCode, key) => {
+    console.log(keyCode, key);
+  };
+
   return (
-    <div className="app" data-theme={isDarkMode ? "dark" : ""} >
-
+    <div
+      className="app"
+      tabIndex="0"
+      onKeyDown={(event) => handleKeyPress(event.keyCode, event.key)}
+      data-theme={isDarkMode ? "dark" : ""}
+    >
       <div className="app_calculator">
-
         <div className="app_calculator_navbar">
+          <img src={isDarkMode ? moon : sun} alt="mode" />
 
           <div
             className="app_calculator_navbar_toggle"
@@ -25,16 +32,14 @@ function App() {
           >
             <div
               className={`app_calculator_navbar_toggle_circle ${
-                isDarkMode ? "app_calculator_navbar_toggle_circle_active" : " "}`}
- >
-              {/* <img src={isDarkMode ? moon : sun} alt="mode" /> */}
-            </div>
+                isDarkMode ? "app_calculator_navbar_toggle_circle_active" : " "
+              }`}
+            ></div>
           </div>
         </div>
-        
+
         <Header />
-        <Keypad />
-          
+        <KeyPad handleKeyPress={handleKeyPress} />
       </div>
     </div>
   );
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,14 +1,30 @@
-import React from 'react'
-import './Header.css';
+import React, { useEffect, useRef } from "react";
+import "./Header.css";
 
- const  Header = () => {
-
-
+const Header = () => {
+  const resultRef = useRef();
+  useEffect(() => {
+    resultRef.current.scrollIntoView({ behavior: "smooth" });
+  }, []);
 
   return (
-    <div className='header'>
-        <h1>Header</h1>
+    <div className="header custom-scroll">
+      <div className="header_history">
+        <p>1+2+3</p>
+        <p>9+2/3</p>
+        <p>1+2+3</p>
+        <p>9+2/3</p>
+        <p>1+2+3</p>
+        <p>9+2/3</p>
+      </div>
+      <br />
+      <div className="header_expression custom-scroll">
+        <p>10+10+10</p>
+        <p ref={resultRef} className="header_result">
+          30
+        </p>
+      </div>
     </div>
-  )
-}
+  );
+};
 export default Header;
